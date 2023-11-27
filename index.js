4d1a836963954f0c180ee70d9b16b0b74c797730
the-usman <2022CS620@student.uet.edu.pk>
2023-11-27 20:32:52 +0500

    Delete Files

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uploadToS3 = async (file) => {
     const s3 = new S3();
     const params = {
         Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `project/usman/${file.originalname}`,  // Corrected 'key' to 'Key'
+        Key: `project/usman/${file.originalname}`,
         Body: file.buffer
     };
     const result = await s3.upload(params).promise();
@@ -40,14 +40,40 @@ app.post('/upload', upload, async (req, res) => {
         const file = req.file;
         const result = await uploadToS3(file);
         res.json({
-            success: true, 
-            message: "Uploaded Successfully"
+            success: true,
+            message: "Uploaded Successfully",
+            result
         });
     } catch (error) {
         console.error('Error uploading to S3:', error);
         res.status(500).json({
             success: false,
             message: "Error uploading to S3"
+        });
+    }
+});
+
+app.post('/delete', async (req, res) => {
+    try {
+        const s3 = new S3();
+        const url = 'https://sharehive.s3.eu-north-1.amazonaws.com/project/usman/Light+Mode.png';
+        const key = new URL(url).pathname.slice(1);
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Delete: {
+                Objects: [
+                    { Key: "project/usman" }
+                ],
+                Quiet: false
+            }
+        };
+        const result = await s3.deleteObjects(params).promise();
+        res.status(200).json({ result, message: "Successfully deleted" });
+    } catch (error) {
+        console.error('Error to S3:', error);
+        res.status(500).json({
+            success: false,
+            message: "Error Delete to S3"
         });
     }
 });
